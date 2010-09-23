72e41779a0e8ba95f9cf4402ec985a3a9b74e391
Nicolas Traeder <matic@maticsbase.(none)>
2010-09-23 17:49:24 +0200

    updated the plugin for Croogo 1.3.2

--- sitemap/config/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-    CroogoRouter::connect('/sitemap', array('plugin' => 'sitemap', 'controller' => 'sitemap', 'action' => 'index'));
-    CroogoRouter::connect('/sitemap/*', array('plugin' => 'sitemap', 'controller' => 'sitemap', 'action' => 'index'));
-    CroogoRouter::connect('/sitemap.xml', array('plugin' => 'sitemap', 'controller' => 'sitemap', 'action' => 'xml'));
-    
-    Router::parseExtensions();
-    
-?>
